48954217671a65248795a43d8751b3423e6db951
yourtulloh <git@drivecok.eu.org>
2023-12-12 02:24:35 +0800

    ...
    Signed-off-by: yourtulloh <git@drivecok.eu.org>

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ read header_name
 echo "Input Logo Name:"
 read logo_name
 
-echo "\n\ncowsay -f eyes $header_name | lolcat\nfiglet $logo_name | lolcat\ndate | lolcat\npython3 bw.py | lolcat" >> ~/.bashrc
+echo -e "\n\ncowsay -f eyes $header_name | lolcat\nfiglet $logo_name | lolcat\ndate | lolcat\npython3 bw.py | lolcat" >> ~/.bashrc
 
 echo "homeVPS Installed successfully. Now reconnect your VPS to take effect!"
