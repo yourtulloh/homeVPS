c4d0c2eab92b7480edce94de9d07c3476a535546
yourtulloh <git@drivecok.eu.org>
2024-10-15 19:26:04 +0800

    syang
    Signed-off-by: yourtulloh <git@drivecok.eu.org>

--- install.sh
+++ install.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
+
+# Bersihkan terminal
 clear
-echo "Installing pkg: python3 python3-pip figlet cowsay lolcat neofetch ... Please wait!"
-apt install -y python3 python3-pip figlet cowsay lolcat neofetch > /dev/null
+
+# Update dan upgrade paket sistem
+echo "Menjalankan apt update & upgrade... Mohon tunggu!"
+sudo apt update && sudo apt upgrade -y > /dev/null
+
+# Instal paket yang diperlukan
+echo "Menginstal paket yang dibutuhkan... Mohon tunggu!"
+sudo apt install -y python3 python3-pip docker-compose figlet cowsay lolcat neofetch git curl certbot nginx python3-certbot-nginx timedatectl > /dev/null
+
+# Unduh skrip Python
 curl -o ~/bw.py -O https://raw.githubusercontent.com/yourtulloh/homeVPS/master/bw.py -s > /dev/null
-clear
-echo "Pkg Installed successfully!" | lolcat
-echo "Input Header Name:" | lolcat
+
+# Set zona waktu
+sudo timedatectl set-timezone Asia/Jakarta
+
+# Pesan konfirmasi
+echo "Paket telah berhasil diinstal!" | lolcat
+
+# Minta input nama header dan logo dari pengguna
+echo "Masukkan Nama Header:" | lolcat
 read header_name
-echo "Input Logo Name:" | lolcat
+echo "Masukkan Nama Logo:" | lolcat
 read logo_name
 
-echo -e "\n\ncowsay -f eyes $header_name | lolcat\nfiglet $logo_name | lolcat\ndate | lolcat\npython3 bw.py | lolcat" >> ~/.bashrc
+# Tambahkan perintah ke .bashrc
+echo -e "\ncowsay -f eyes $header_name | lolcat\nfiglet $logo_name | lolcat\ndate | lolcat\npython3 ~/bw.py | lolcat" >> ~/.bashrc
+
+# Tampilkan pesan sukses
 figlet HomeVPS | lolcat
-echo "Installed successfully. Now reconnect your VPS to take effect!" | lolcat
+echo "Instalasi berhasil. Silakan sambungkan kembali VPS Anda agar perubahan berlaku!" | lolcat
+
+# Bersihkan terminal dan jalankan skrip Python
 clear
-python3 bw.py
+python3 ~/bw.py
